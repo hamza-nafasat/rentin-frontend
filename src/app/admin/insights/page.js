0e87f5df871a7e65c7ccb35212c447819eb80506
bilaltetraintech <bilal@tetraintech.com>
2025-05-06 16:52:59 +0500

    working onadmin flow

--- src/app/admin/insights/page.js
+++ src/app/admin/insights/page.js
@@ -1,7 +1,40 @@
+import TopCards from '@/components/admin/properties/TopCards';
+import InquiryRate from '@/components/owner/insights/InquiryRate';
+import InsightEarning from '@/components/owner/insights/InsightEarning';
+import PropertyPerformance from '@/components/owner/insights/PropertyPerformance';
+import TenantDisputes from '@/components/owner/insights/TenantDisputes';
+import TenantIntent from '@/components/owner/insights/TenantIntent';
+import TotalRevenue from '@/components/owner/insights/TotalRevenue';
+import PaymentTransactionHistory from '@/components/owner/payments/PaymentTransactionHistory';
+import { earningsData, insightCardsData, tenantIncomeLegendLabels, tenantIntent } from '@/data/data';
 import React from 'react';
 
 function AdminInsights() {
-  return <div>AdminInsights</div>;
+  return (
+    <div className="flex h-full flex-col gap-4">
+      <h3 className="text-textColor text-lg font-semibold md:text-[22px]">Insight Details</h3>
+      <TopCards data={insightCardsData} />
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-12">
+        <div className="flex h-[247px] grid-cols-12 rounded-md bg-white shadow-md lg:col-span-4">
+          <TenantIntent title="Tenant Intent" data={tenantIntent} legend={tenantIncomeLegendLabels} />
+        </div>
+        <div className="flex h-[247px] grid-cols-12 rounded-md bg-white shadow-md lg:col-span-4">
+          <TotalRevenue />
+        </div>
+        <div className="flex h-[247px] grid-cols-12 rounded-md bg-white shadow-md lg:col-span-4">
+          <InquiryRate />
+        </div>
+      </div>
+      <div className="grid w-full grid-cols-1">
+        <div className="rounded-md bg-white">
+          <PaymentTransactionHistory />
+        </div>
+      </div>
+      <div className="grid h-[370px] grid-cols-1 rounded-md bg-white p-5 shadow-md">
+        <InsightEarning data={earningsData} />
+      </div>
+    </div>
+  );
 }
 
 export default AdminInsights;
--- src/app/admin/payments/page.js
+++ src/app/admin/payments/page.js
@@ -1,7 +1,45 @@
+import EarningsChart from '@/components/owner/payments/EarningsChart';
+import PaymentOverview from '@/components/owner/payments/PaymentOverview';
+import PaymentTransactionHistory from '@/components/owner/payments/PaymentTransactionHistory';
+import Sales from '@/components/owner/payments/Sales';
+import Stripe from '@/components/owner/payments/Stripe';
+import TenantPayment from '@/components/owner/payments/TenantPayment';
+import TransactionHistory from '@/components/owner/payments/TransactionHistory';
 import React from 'react';
 
 function AdminPayment() {
-  return <div>AdminPayment</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <h3 className="text-textColor text-lg font-semibold md:text-[22px]">Transactions Details</h3>
+      <div className="grid w-full grid-cols-12 gap-4">
+        <div className="col-span-12 rounded-md bg-white lg:col-span-7">
+          <EarningsChart />
+        </div>
+        <div className="col-span-12 rounded-md bg-white lg:col-span-5">
+          <Stripe />
+        </div>
+      </div>
+      <div className="grid w-full grid-cols-12 gap-4">
+        <div className="col-span-12 rounded-md bg-white md:col-span-6 lg:col-span-3">
+          <PaymentOverview />
+        </div>
+        <div className="col-span-12 rounded-md bg-white md:col-span-6 lg:col-span-3">
+          <TransactionHistory />
+        </div>
+        <div className="col-span-12 rounded-md bg-white md:col-span-6 lg:col-span-3">
+          <Sales />
+        </div>
+        <div className="col-span-12 rounded-md bg-white md:col-span-6 lg:col-span-3">
+          <TenantPayment />
+        </div>
+      </div>
+      <div className="grid w-full grid-cols-1">
+        <div className="rounded-md bg-white">
+          <PaymentTransactionHistory />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AdminPayment;
